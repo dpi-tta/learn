dd40db54b6b0e376dd7fa0e3bb875a5a1063137f
Ian Heraty <heratyian@gmail.com>
2025-06-04 10:39:13 -0500

    Add lesson styling and links to headers

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -4,4 +4,15 @@ module MarkdownHelper
     markdown = Redcarpet::Markdown.new(renderer, extensions = {})
     markdown.render(text).html_safe
   end
+
+  def render_lesson_markdown(text)
+    renderer = LessonMarkdownRenderer.new(with_toc_data: true)
+    markdown = Redcarpet::Markdown.new(
+      renderer,
+      autolink: true,
+      fenced_code_blocks: true,
+      tables: true
+    )
+    markdown.render(text).html_safe
+  end
 end
