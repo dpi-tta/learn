00f313fcac231ac2d563472c4237a6fe9e2e50ab
Ian Heraty <heratyian@gmail.com>
2025-06-16 17:44:43 -0500

    Add seeds courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,7 +79,93 @@ html_css_slugs.each_with_index do |slug, index|
   end
 end
 
-# TODO: capstone course
-# TODO: data structures and algorithms course
+data_structures_and_algorithms_course = Course.find_or_create_by(
+  title: "Data Structures, Algorithms, and Acing Coding Interviews",
+  description: <<~DESCRIPTION
+    This course focuses on fundamental data structures and algorithms, as well as techniques for acing coding interviews.
+    You will practice solving common algorithmic problems and learn strategies to approach and solve these problems effectively.
+    By the end of this module, you will be able to: Understand and implement common data structures, Solve algorithmic problems using efficient techniques, Prepare for technical coding interviews, Demonstrate problem-solving skills and algorithmic thinking.
+  DESCRIPTION
+)
+
+data_structures_and_algorithms_slugs = [
+  "ruby-data-structures-algorithms-intro",
+  "ruby-data-structures-algorithms-two-sum",
+  "ruby-data-structures-algorithms-most-frequent-element-in-array",
+  "ruby-data-structures-algorithms-longest-consecutive-sequence",
+  "ruby-data-structures-algorithms-balanced-parentheses",
+  "ruby-data-structures-algorithms-palindrome",
+  "ruby-data-structures-algorithms-climbing-stairs",
+  "ruby-data-structures-algorithms-kth-largest-element",
+  "ruby-data-structures-algorithms-first-unique-character-in-string",
+  "ruby-data-structures-algorithms-find-height-of-binary-tree",
+  "ruby-data-structures-algorithms-reverse-linked-list"
+]
+
+data_structures_and_algorithms_slugs.each_with_index do |slug, index|
+  lesson = Lesson.find_by(slug: slug)
+  if lesson
+    CourseLesson.find_or_create_by!(course: data_structures_and_algorithms_course, lesson: lesson) do |cl|
+      cl.position = index
+    end
+    puts "Added #{lesson.slug} to #{data_structures_and_algorithms_course.title}"
+  else
+    puts "⚠️ Lesson not found: #{slug}"
+  end
+end
+
+capstone_course = Course.find_or_create_by(
+  title: "Capstone Project",
+  description: <<~DESCRIPTION
+    This course is the culmination of your learning journey, where you will apply all the skills acquired throughout the program to design, build, and present your own software project.
+    From ideation to deployment, you will learn how to conceptualize a project, write functional requirements, and bring your idea to life.
+    The course guides you through noticing good ideas, writing a functional specification, coding, deploying, and preparing a professional presentation to showcase your project.
+
+    ## Learning Objectives:
+
+    - Identify and develop a project idea based on real-world problems or creative inspiration.
+    - Write clear functional requirements and specifications to define the scope and features of your project.
+    - Translate project specifications into working code.
+    - Deploy your application to a production environment.
+    - Prepare and deliver a polished presentation of your project, showcasing its features and technical implementation.
+  DESCRIPTION
+)
+
+capstone_slugs = [
+  "building-your-own-idea",
+  "noticing-a-project-idea",
+  "identifying-pain-points",
+  "gathering-user-feedback",
+  "writing-functional-requirements",
+  "keep-it-simple",
+  "getting-started-coding-your-project",
+  # TODO: deploying to Render
+  "estimating-and-prioritizing-work",
+  "presenting-your-project",
+  "how-to-get-your-first-5-users"
+]
+
+capstone_slugs.each_with_index do |slug, index|
+  lesson = Lesson.find_by(slug: slug)
+  if lesson
+    CourseLesson.find_or_create_by!(course: capstone_course, lesson: lesson) do |cl|
+      cl.position = index
+    end
+    puts "Added #{lesson.slug} to #{capstone_course.title}"
+  else
+    puts "⚠️ Lesson not found: #{slug}"
+  end
+end
+
+# TODO: VS Code & Terminal Essentials
+# TODO: Ruby
+# TODO: Writing Our Own Programs
+# TODO: Interviewing
+# TODO: HTTP Requests & APIs
+# TODO: Web Apps
+# TODO: Database Architecture
+# TODO: GitHub Workflow
+# TODO: Full Stack Web Apps
+# TODO: Industrial
 
 puts "✅ Done seeding courses and lessons."
