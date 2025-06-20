0dd8c267926fb2d895b5a7da2bd2697d23340cf4
Ian Heraty <heratyian@gmail.com>
2025-06-20 11:04:02 -0500

    Reduce header sizing in lesson style

--- app/renderers/lesson_style_config.rb
+++ app/renderers/lesson_style_config.rb
@@ -1,9 +1,9 @@
 class LessonStyleConfig
   def self.heading_classes
     {
-      "h1" => "display-1 mt-5",
-      "h2" => "display-2 mt-5",
-      "h3" => "display-4 mt-5",
+      "h1" => "display-4 mt-5",
+      "h2" => "display-5 mt-5",
+      "h3" => "display-6 mt-5",
       "h4" => "mb-2",
       "h5" => "mb-1",
       "h6" => "mb-1"
